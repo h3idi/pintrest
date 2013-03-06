75ba54f99f612c3bf40b0c59f7c82595f48b4eb4
h3idi <heidi.harman@gmail.com>
2013-03-06 21:37:17 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pintrest::Application.config.secret_token = 'b33563e04a5ffbf5e111bebf3a8841bee109eeb77e038473051122bdf6beebd7e4f5b00811a6b5e34ace73703c2a6c5138cae06a685c5062ad53f4121d3ae278'
